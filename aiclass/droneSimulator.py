abcd2be06d318c76ea8317d3e8c66c508fd66411
lae111 <logan.a.elliott-1@ou.edu>
2020-03-07 15:49:55 -0600

    Adding asteroid points key

--- aiclass/droneSimulator.py
+++ aiclass/droneSimulator.py
@@ -87,6 +87,12 @@ class Asteroid:
         else:
             self.velocity = Velocity(0, 0, 0, 0)
 
+        if (self.is_mineable):
+            # https://stackoverflow.com/questions/41383849/setting-the-fill-of-a-rectangle-drawn-with-canvas-to-an-rgb-value
+            fill_color = "#%02x%02x%02x" % (0, int(self.resources * 256), int(self.resources * 256))
+        else:
+            fill_color = "#B59892"
+
 class Drone:
     def __init__(self, position, id, team_color, tello):
         """
